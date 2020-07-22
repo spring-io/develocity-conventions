4d3698c6784dbc29cd159ff3374d14657c1b91fe
Andy Wilkinson <awilkinson@pivotal.io>
2020-07-22 10:29:40 +0100

    Update CI pipeline to enable publishing to Maven Central
    Closes gh-8

--- ci/scripts/promote.sh
+++ ci/scripts/promote.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-set -e
 
 source $(dirname $0)/common.sh
 
 buildName=$( cat artifactory-repo/build-info.json | jq -r '.buildInfo.name' )
 buildNumber=$( cat artifactory-repo/build-info.json | jq -r '.buildInfo.number' )
+groupId=$( cat artifactory-repo/build-info.json | jq -r '.buildInfo.modules[0].id' | sed 's/\(.*\):.*:.*/\1/' )
 version=$( cat artifactory-repo/build-info.json | jq -r '.buildInfo.modules[0].id' | sed 's/.*:.*:\(.*\)/\1/' )
 
 if [[ $RELEASE_TYPE = "RELEASE" ]]; then
@@ -24,7 +24,53 @@ curl \
 	-d "{\"status\": \"staged\", \"sourceRepo\": \"libs-staging-local\", \"targetRepo\": \"${targetRepo}\"}"  \
 	-f \
 	-X \
-	POST "${ARTIFACTORY_SERVER}/api/build/promote/${buildName}/${buildNumber}" > /dev/null || { echo "Failed to promote" >&2; exit 1; }
+	POST "${ARTIFACTORY_SERVER}/api/build/promote/${buildName}/${buildNumber}" > /dev/null || {
+		result=$( curl -s -f -u ${ARTIFACTORY_USERNAME}:${ARTIFACTORY_PASSWORD} "${ARTIFACTORY_SERVER}/api/build/${buildName}/${buildNumber}" )
+		resultRepo=$( echo $result | jq -r '.buildInfo.statuses[0].repository' )
+		if [[ $resultRepo = "libs-release-local" ]]; then
+			echo "Already promoted"
+		else
+			echo "Failed to promote" >&2
+			exit 1
+		fi
+	}
+
+if [[ $RELEASE_TYPE = "RELEASE" ]]; then
+	curl \
+		-s \
+		--connect-timeout 240 \
+		--max-time 2700 \
+		-u ${ARTIFACTORY_USERNAME}:${ARTIFACTORY_PASSWORD} \
+		-H "Content-type:application/json" \
+		-d "{\"sourceRepos\": [\"libs-release-local\"], \"targetRepo\" : \"spring-distributions\", \"async\":\"true\"}" \
+		-f \
+		-X \
+		POST "${ARTIFACTORY_SERVER}/api/build/distribute/${buildName}/${buildNumber}" > /dev/null || { echo "Failed to distribute" >&2; exit 1; }
+
+	echo "Waiting for artifacts to be published"
+
+	WAIT_TIME=20
+	WAIT_ATTEMPTS=120
+
+	artifacts_published=false
+	retry_counter=0
+	while [ $artifacts_published == "false" ] && [ $retry_counter -lt $WAIT_ATTEMPTS ]; do
+		result=$( curl -s -f -u ${BINTRAY_USERNAME}:${BINTRAY_API_KEY} https://api.bintray.com/packages/"${BINTRAY_SUBJECT}"/"${BINTRAY_REPO}"/"${groupId}" )
+		if [ $? -eq 0 ]; then
+			versions=$( echo "$result" | jq -r '.versions' )
+			exists=$( echo "$versions" | grep "$version" -o || true )
+			if [ "$exists" = "$version" ]; then
+				artifacts_published=true
+			fi
+		fi
+		retry_counter=$(( retry_counter + 1 ))
+		sleep $WAIT_TIME
+	done
+	if [[ $artifacts_published = "false" ]]; then
+		echo "Failed to publish"
+		exit 1
+	fi
+fi
 
 echo "Promotion complete"
 echo $version > version/version
