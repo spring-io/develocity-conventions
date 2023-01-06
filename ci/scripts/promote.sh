9955a7c951c94ae6a77546aaad8936f5a6376b44
Andy Wilkinson <wilkinsona@vmware.com>
2023-01-06 11:33:49 +0000

    Publish releases to Maven Central
    Closes gh-29
    Closes gh-58

--- ci/scripts/promote.sh
+++ ci/scripts/promote.sh
@@ -1,39 +1,17 @@
 #!/bin/bash
 
-source $(dirname $0)/common.sh
+CONFIG_DIR=git-repo/ci/config
 
-buildName=$( cat artifactory-repo/build-info.json | jq -r '.buildInfo.name' )
-buildNumber=$( cat artifactory-repo/build-info.json | jq -r '.buildInfo.number' )
-groupId=$( cat artifactory-repo/build-info.json | jq -r '.buildInfo.modules[0].id' | sed 's/\(.*\):.*:.*/\1/' )
 version=$( cat artifactory-repo/build-info.json | jq -r '.buildInfo.modules[0].id' | sed 's/.*:.*:\(.*\)/\1/' )
+export BUILD_INFO_LOCATION=$(pwd)/artifactory-repo/build-info.json
 
-if [[ $RELEASE_TYPE = "RELEASE" ]]; then
-	targetRepo="libs-release-local"
-else
-	echo "Unknown release type $RELEASE_TYPE" >&2; exit 1;
-fi
+java -jar /concourse-release-scripts.jar \
+  --spring.config.location=${CONFIG_DIR}/release-scripts.yml \
+  publishToCentral $RELEASE_TYPE $BUILD_INFO_LOCATION artifactory-repo || { exit 1; }
 
-echo "Promoting ${buildName}/${buildNumber} to ${targetRepo}"
-
-curl \
-	-s \
-	--connect-timeout 240 \
-	--max-time 900 \
-	-u ${ARTIFACTORY_USERNAME}:${ARTIFACTORY_PASSWORD} \
-	-H "Content-type:application/json" \
-	-d "{\"status\": \"staged\", \"sourceRepo\": \"libs-staging-local\", \"targetRepo\": \"${targetRepo}\"}"  \
-	-f \
-	-X \
-	POST "${ARTIFACTORY_SERVER}/api/build/promote/${buildName}/${buildNumber}" > /dev/null || {
-		result=$( curl -s -f -u ${ARTIFACTORY_USERNAME}:${ARTIFACTORY_PASSWORD} "${ARTIFACTORY_SERVER}/api/build/${buildName}/${buildNumber}" )
-		resultRepo=$( echo $result | jq -r '.buildInfo.statuses[0].repository' )
-		if [[ $resultRepo = "libs-release-local" ]]; then
-			echo "Already promoted"
-		else
-			echo "Failed to promote" >&2
-			exit 1
-		fi
-	}
+java -jar /concourse-release-scripts.jar \
+  --spring.config.location=${CONFIG_DIR}/release-scripts.yml \
+  promote $RELEASE_TYPE $BUILD_INFO_LOCATION || { exit 1; }
 
 echo "Promotion complete"
 echo $version > version/version
